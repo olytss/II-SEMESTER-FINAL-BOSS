69a293547f712dc296f38c1434bb5e1067fd48d8
olytss <teaforclass@gmail.com>
2026-05-18 22:57:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,146 @@
+﻿using System.Data;
+
+namespace meow
+{
+    internal class programm
+    {
+        static private void Main(string[] args)
+        {
+            CustomDate.NewDate();
+            Console.WriteLine(CustomDate.isLeapYear(CustomDate.dates[0]));
+            Console.WriteLine(CustomDate.HowManyDaysInMonths(CustomDate.dates[0]));
+        }
+    }
+    public class CustomDate
+    {
+        static public List<CustomDate> dates = new List<CustomDate>();
+        private int _day; //backing field
+        private int Day {get { return _day; } set 
+            {
+                if (value < 0 || value > 31)
+                {
+                    throw new ArgumentOutOfRangeException("No"); //xd first time seeing Visual Studio tabulating throw exception
+                }
+                else
+                {
+                    _day = value; //ALWAYS ASSING TO THE BACKING FIELD!!!!!!!!!!!!!!!!!!
+                }
+            
+            } 
+        
+        }
+        private int _month;
+        public int Month
+        {
+            get 
+            { return _month; }
+            set
+            {
+                if (value < 0)
+                {
+                   throw new ArgumentOutOfRangeException("Cant be less than 0 MONTH");
+                }
+                else
+                {
+                    _month = value;
+                }
+            }
+        }
+        private int _year;
+        public int Year
+        {
+            get
+            {
+                return _year;
+
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Can be less than 0");
+                }
+                else
+                {
+                    _year = value;
+                }
+
+            }
+
+        }
+        
+
+        // hehehehehh six seven
+        CustomDate(int day, int month, int year)
+        {
+            if(month < 1 || month > 12)
+            {
+                throw new ArgumentOutOfRangeException("MONTH", "OUT OF RANGE");
+            }
+            this.Day = day;
+            this.Month = month;
+            this.Year = year;
+        }
+        static public void NewDate()
+        {
+            int day = 0;
+            int month = 0;
+            int year = 0;
+            bool exception = false;
+            Console.WriteLine("Give me day, month and year");
+            do
+            {
+                try
+                {
+                    day = int.Parse(Console.ReadLine());
+                    month = int.Parse(Console.ReadLine());
+                    year = int.Parse(Console.ReadLine());
+                    
+                    CustomDate date = new CustomDate(day, month, year);
+                    dates.Add(date);
+                    exception = true;
+                }
+                catch (FormatException ex)
+                {
+
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+                catch (ArgumentOutOfRangeException ex1)
+                {
+                    Console.WriteLine("out of range buddy");
+                    continue;
+                }
+
+
+            } while (!exception);
+        }
+        static public bool isLeapYear(CustomDate Date)
+        {
+            return DateTime.IsLeapYear(Date.Year);
+        }
+        static public int HowManyDaysInMonths(CustomDate date)
+        {
+            return DateTime.DaysInMonth(date.Year, date.Month);
+        }
+        static public bool isLeapYear(int year)
+        {
+            return DateTime.IsLeapYear(year);
+        }
+
+        static public bool isLeapYear(string yearText)
+        {
+            int parsedYear = int.Parse(yearText);
+            return DateTime.IsLeapYear(parsedYear);
+        }
+
+        static public int HowManyDaysInMonths(int year, int month)
+        {
+            return DateTime.DaysInMonth(year, month);
+        }
+        static public int HowManyDaysInMonths(string fullDateString)
+        {
+            DateTime parsedDate = DateTime.Parse(fullDateString);
+            return DateTime.DaysInMonth(parsedDate.Year, parsedDate.Month);
+        }
+    }
+}
---
+++ part 2.cs
@@ -0,0 +1,230 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace meow
+{
+    internal class Mewo
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome!");
+                Console.WriteLine("1. Add new Team Member \n 2. Delete Team Member \n 3. Middle Salary Value \n 4. Number of Technical Team Members");
+                ConsoleKeyInfo cki = Console.ReadKey(true);
+                switch (cki.Key)
+                {
+                    case ConsoleKey.D1:
+                        Team.NewTeamMember();
+                        break;
+
+                    case ConsoleKey.D2:
+                        Team.DeleteMember();
+                        break;
+
+                    case ConsoleKey.D3:
+                        Console.WriteLine($"Middle value is {Team.commandSalary()}");
+                        Console.ReadLine();
+                        break;
+
+                    case ConsoleKey.D4:
+                        break;
+                    default:
+                        continue;
+                        
+                }
+            }
+        }
+    }
+    public class Team
+    {
+        static public List<Team> TeamList = new List<Team>();
+        private string _name;
+        private string _surname;
+        private string _department;
+        private long _salary;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value == "" || value == null)
+                {
+                    throw new FormatException("Wrong Format");
+                }
+                else
+                {
+                    _name = value;
+                }
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                if (value == "" || value == null)
+                {
+                    throw new FormatException("Wrong Format");
+                }
+                else
+                {
+                    _surname = value;
+                }
+            }
+        }
+        public string Department
+        {
+            get
+            {
+                return _department;
+            }
+            set
+            {
+                 value = value.ToLower();
+                if (value == "technical team" || value == "social team")
+                {
+                    _department = value;
+                }
+                else
+                {
+                    throw new Exception("No such department");
+                }
+            }
+        }
+        public long Salary
+        {
+            get
+            {
+                return _salary;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    throw new ArgumentOutOfRangeException("Ivalid Value");
+                }
+                else
+                {
+                    _salary = value;
+                }
+            }
+        }
+        public Team(string name, string surname, string department, long salary)
+        {
+            Name = name;
+            Surname = surname;
+            Department = department;
+            Salary = salary;
+        }
+
+        static public void NewTeamMember()
+        {
+            
+            {
+                bool isValid = false;
+                do
+                {
+                    Console.Clear();
+                    try
+                    {
+                       
+                        Console.WriteLine("Input name, surname, department and salary");
+                        string nameTemp = Console.ReadLine();
+                        string surnameTemp = Console.ReadLine();
+                        Console.WriteLine("Departments: \n Social Team");
+
+                        string departmentTemp = Console.ReadLine();
+                        long salaryTemp = long.Parse(Console.ReadLine());
+
+                        Team teamMember = new Team(nameTemp, surnameTemp, departmentTemp, salaryTemp);
+                        TeamList.Add(teamMember);
+                        isValid = true;
+                    }catch (ArgumentOutOfRangeException ex)
+                    {
+                        Console.WriteLine("Argument out of Range" + ex.Message);
+                    }
+                    catch(FormatException ex1) 
+                    {
+                        Console.WriteLine("Format Exception" + ex1.Message);
+                    }
+                    
+                }
+                while (!isValid);
+
+            }
+        }
+        static public void DeleteMember()
+        {
+            if(TeamList.Count == 0) return;
+            foreach (var item in TeamList)
+            {
+                Console.WriteLine("----------------------------");
+                Console.WriteLine($"Name: {item.Name} \n Surname: {item.Surname} \n Department: {item.Department} \n Salary {item.Salary}");
+                Console.WriteLine("----------------------------");
+            }
+
+            Console.WriteLine("What member of the Team do you want to delete");
+            int index = 0;
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out index))
+                {
+                    Console.WriteLine("Input invalid");
+                }
+                else
+                {
+                    while (true)
+                    {
+                        if(index > Team.TeamList.Count)
+                        {
+                            Console.WriteLine("No");
+                            continue;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    break;
+                }
+               
+            }
+            TeamList.RemoveAt(index - 1);
+            Console.WriteLine("Removed!");
+        }
+       
+        static public long commandSalary()
+        {
+            if(TeamList.Count == 0) return 0;
+            long salaryCombined= 0;
+            long middleValue = 0;
+            foreach (var person in TeamList)
+            {
+                salaryCombined += person.Salary;
+            }
+            middleValue = salaryCombined / TeamList.Count;
+            return middleValue;
+        }
+
+    }
+    public class TechnicalTeam : Team
+    {
+        public TechnicalTeam(string name, string surname, string department, long salary)   
+                            : base (name, surname, department, salary)
+        {
+             this.Name = name;
+            this.Surname = surname;
+            this.Department = department;
+            this.Salary = salary;
+        }
+    }
+
+}
